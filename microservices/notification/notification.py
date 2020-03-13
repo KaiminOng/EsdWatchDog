0e969ac33d5b178a5ea88d9682d803a3a28a7e61
ongkaimin <kaimin.ong.2018@sis.smu.edu.sg>
2020-03-13 15:04:49 +0800

    add initial files

---
+++ microservices/notification/notification.py
@@ -0,0 +1,24 @@
+import requests
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+@app.route("/send_message/<bot_chatID>", methods=['POST'])
+def telegram_bot_sendtext(bot_chatID):
+    
+    bot_token = '1129690128:AAFzGAL-Rur8QAZyjG2_62f5tvQvOKjv29w'
+    # bot_chatID = '-1001260714304'
+    bot_message = "TESTING EH"
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+    
+# if __name__ == '__main__':
+#     app.run(port=300, debug=True)
+
+test = telegram_bot_sendtext("-393119922")
+print(test)
