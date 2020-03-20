a81381388681a5c849077b0a0d2b68378b0f322b
nadiyams <nadiyas.2018@smu.edu.sg>
2020-03-20 14:24:51 +0800

    update quick-check.py

--- microservices/ping/quick-check.py
+++ microservices/ping/quick-check.py
@@ -1,4 +1,4 @@
-import os, time, http.client
+import os, time, http.client, asyncio
 from termcolor import colored
 
 SITES = [
@@ -6,18 +6,33 @@ SITES = [
 	"www.facebook.com",	
 ]
 
-while 1:
-	for site in SITES:
-		conn = http.client.HTTPConnection(site, timeout=10)
-		conn.request("HEAD", "/")
-		response = conn.getresponse()
+# while 1:
+# 	for site in SITES:
+# 		conn = http.client.HTTPConnection(site, timeout=10)
+# 		conn.request("HEAD", "/")
+# 		response = conn.getresponse()
 		
-		if response.status != 200:
-			print("\a")
-			response.status = colored(response.status, 'red')
+# 		if response.status != 200:
+# 			print("\a")
+# 			response.status = colored(response.status, 'red')
 					
-		print("{0:30} {1:10} {2:10}".format(site, response.status, response.reason))
-		conn.close()
+# 		print("{0:30} {1:10} {2:10}".format(site, response.status, response.reason))
+# 		conn.close()
 	
-	time.sleep(2)
-	os.system("clear")
+# 	time.sleep(2)
+# 	os.system("clear")
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
