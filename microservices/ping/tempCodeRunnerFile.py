3268679084efd2f8a5d31cb0b789b3b08570d6fc
NGOH Wei Jie <weijie.ngoh.2018@sis.smu.edu.sg>
2020-03-21 12:28:48 +0800

    Created quick-check2.py (ping 2.0)
    Another method for ping micro service, which takes into account failed connection

--- microservices/ping/tempCodeRunnerFile.py
+++ microservices/ping/tempCodeRunnerFile.py
@@ -1,7 +1,2 @@
-
-if __name__ == "__main__":
-    import time
-    s = time.perf_counter()
-    asyncio.run(main())
-    elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+            # except aiohttp.ClientError:
+            #     results.append(False)
