49cf15e65f1aefc01ebd8d53323c10b32f5899af
DisarmingSnail <kohwayne@live.com.sg>
2020-03-25 22:30:58 +0800

    Add character limit when creating varchar column

--- microservices/dataHandler/dataHandler/models.py
+++ microservices/dataHandler/dataHandler/models.py
@@ -1,10 +1,10 @@
-from dataHandler import db
+from . import db
 
 # Define models
 
 account_endpoint = db.Table('user_endpoints', 
-    db.Column('account_id', db.String, db.ForeignKey('account.id'), primary_key=True),
-    db.Column('endpoint_url', db.String, db.ForeignKey('endpoint.endpoint_url'), primary_key=True)
+    db.Column('account_id', db.String(120), db.ForeignKey('account.id'), primary_key=True),
+    db.Column('endpoint_url', db.String(120), db.ForeignKey('endpoint.endpoint_url'), primary_key=True)
 )
 
 endpoint_contact = db.Table('table_contacts', 
@@ -16,9 +16,9 @@ class Account(db.Model):
 
     __tablename__ = 'account'
 
-    id = db.Column(db.String, primary_key = True)
+    id = db.Column(db.String(120), primary_key = True)
     username = db.Column(db.String(80), nullable = False)
-    endpoints = db.relationship('Endpoint', secondary=account_endpoint, lazy='subquery', backref=db.backref('accounts', lazy=True))
+    endpoints = db.relationship('Endpoint', secondary=account_endpoint, lazy='dynamic', backref=db.backref('accounts', lazy=True))
 
     def __init__(self, id, username):
         self.id = id
@@ -33,7 +33,7 @@ class Endpoint(db.Model):
     __tablename__ = 'endpoint'
 
     endpoint_url = db.Column(db.String(120), primary_key = True, nullable = False)
-    contacts = db.relationship('Contact', secondary=endpoint_contact, lazy='subquery', backref=db.backref('endpoints', lazy=True))
+    contacts = db.relationship('Contact', secondary=endpoint_contact, lazy='dynamic', backref=db.backref('endpoints', lazy=True))
 
     def __init__(self, endpoint):
         self.endpoint = endpoint
@@ -43,4 +43,7 @@ class Contact(db.Model):
 
     __tablename__ = 'contact'
 
-    chat_id = db.Column(db.String(), primary_key = True, nullable = False)
+    def __init__(self, tg_chat_id):
+        self.chat_id = tg_chat_id
+
+    chat_id = db.Column(db.String(120), primary_key = True, nullable = False)
