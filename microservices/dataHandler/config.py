c169e7bb3a26149af226600abeee1ce66336a437
DisarmingSnail <kohwayne@live.com.sg>
2020-03-25 17:09:18 +0800

    Add models and config files for dataHandler

---
+++ microservices/dataHandler/config.py
@@ -0,0 +1,20 @@
+from os import environ
+
+
+class Config:
+
+    # Configuration variables for database ; Will include them as environment variables
+    sql_hostname = 'localhost'
+    sql_user = 'root'
+    sql_port = 3306
+    sql_password = 'password'
+    db_name = 'watchdog'
+
+    """Set configration vars"""
+    # Database env vars
+    environ['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{sql_user}:{sql_password}@{sql_hostname}:{sql_port}/{db_name}'
+    environ['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+    
+    # Database 
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
---
+++ microservices/dataHandler/dataHandler/models.py
@@ -0,0 +1,46 @@
+from dataHandler import db
+
+# Define models
+
+account_endpoint = db.Table('user_endpoints', 
+    db.Column('account_id', db.String, db.ForeignKey('account.id'), primary_key=True),
+    db.Column('endpoint_url', db.String, db.ForeignKey('endpoint.endpoint_url'), primary_key=True)
+)
+
+endpoint_contact = db.Table('table_contacts', 
+    db.Column('endpoint_url', db.String(120), db.ForeignKey('endpoint.endpoint_url'), primary_key = True),
+    db.Column('chat_id', db.String(120), db.ForeignKey('contact.chat_id'), primary_key = True)
+)
+
+class Account(db.Model):
+
+    __tablename__ = 'account'
+
+    id = db.Column(db.String, primary_key = True)
+    username = db.Column(db.String(80), nullable = False)
+    endpoints = db.relationship('Endpoint', secondary=account_endpoint, lazy='subquery', backref=db.backref('accounts', lazy=True))
+
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+    def __repr__(self):
+        return f"User ID: {self.id} Username: {self.username}"
+
+
+class Endpoint(db.Model):
+
+    __tablename__ = 'endpoint'
+
+    endpoint_url = db.Column(db.String(120), primary_key = True, nullable = False)
+    contacts = db.relationship('Contact', secondary=endpoint_contact, lazy='subquery', backref=db.backref('endpoints', lazy=True))
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+
+
+class Contact(db.Model):
+
+    __tablename__ = 'contact'
+
+    chat_id = db.Column(db.String(), primary_key = True, nullable = False)
