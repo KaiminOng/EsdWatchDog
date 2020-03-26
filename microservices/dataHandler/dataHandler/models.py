3857b55afb6755b5160cffe22112338b88beb8e8
DisarmingSnail <kohwayne@live.com.sg>
2020-03-26 19:09:48 +0800

    Remove init methods from model classes

--- microservices/dataHandler/dataHandler/models.py
+++ microservices/dataHandler/dataHandler/models.py
@@ -20,10 +20,6 @@ class Account(db.Model):
     username = db.Column(db.String(80), nullable = False)
     endpoints = db.relationship('Endpoint', secondary=account_endpoint, lazy='dynamic', backref=db.backref('accounts', lazy=True))
 
-    def __init__(self, id, username):
-        self.id = id
-        self.username = username
-
     def __repr__(self):
         return f"User ID: {self.id} Username: {self.username}"
 
@@ -33,17 +29,18 @@ class Endpoint(db.Model):
     __tablename__ = 'endpoint'
 
     endpoint_url = db.Column(db.String(120), primary_key = True, nullable = False)
+    status = db.Column(db.String(20))
+    last_checked = db.Column(db.DateTime)
     contacts = db.relationship('Contact', secondary=endpoint_contact, lazy='dynamic', backref=db.backref('endpoints', lazy=True))
 
-    def __init__(self, endpoint):
-        self.endpoint = endpoint
+    def update_status(self, status, last):
+        """Updates the status and last checked time for endpoint object"""
+        self.status = status
+        self.last_checked = last
 
 
 class Contact(db.Model):
 
     __tablename__ = 'contact'
-
-    def __init__(self, tg_chat_id):
-        self.chat_id = tg_chat_id
 
     chat_id = db.Column(db.String(120), primary_key = True, nullable = False)
