8089af60d08d57445b06e8dea073d2a84e87e27f
DisarmingSnail <kohwayne@live.com.sg>
2020-03-28 00:40:24 +0800

    Add get_or_create helper function

---
+++ microservices/dataHandler/dataHandler/helper.py
@@ -0,0 +1,10 @@
+"""Retrieves data object from table if exists, otherwise create it"""
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.commit()
+        return instance
