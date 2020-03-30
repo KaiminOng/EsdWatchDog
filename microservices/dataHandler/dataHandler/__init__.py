c87081aaeeeb370ee97fb75531e5b5b5b6bf2314
DisarmingSnail <kohwayne@live.com.sg>
2020-03-31 01:22:52 +0800

    Add CORS availability

--- microservices/dataHandler/dataHandler/__init__.py
+++ microservices/dataHandler/dataHandler/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
+    CORS(app)
     app.config.from_object('config.Config')
     db.init_app(app)
 
