ec3fca9024c56599b3131a1e8198f445cb7000b4
DisarmingSnail <kohwayne@live.com.sg>
2020-04-10 22:22:34 +0800

    Add comments for config file

--- webapp/config.php
+++ webapp/config.php
@@ -6,6 +6,9 @@ $bot_token = '1129690128:AAFzGAL-Rur8QAZyjG2_62f5tvQvOKjv29w';
 
 # Domain & Hostname Configuration:
 $domain_name = "esdwatchdog.me";
+
+# Set this variable to the endpoint URI for your kong gateway. Kong will handle routing of requests
+# to the respective backend services
 $hostname = "http://esdwatchdog.me:5001";
 
 ?>
