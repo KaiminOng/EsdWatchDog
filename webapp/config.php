bc0511802a712a5091befc381bc7a491c93544f7
ongkaimin <kaimin.ong.2018@sis.smu.edu.sg>
2020-04-10 21:41:44 +0800

    add config file for hostname and bot config

---
+++ webapp/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+# Bot Configuration: 
+$bot_username = 'ESD_Proj_bot';
+$bot_token = '1129690128:AAFzGAL-Rur8QAZyjG2_62f5tvQvOKjv29w';
+
+# Domain & Hostname Configuration:
+$domain_name = "esdwatchdog.me";
+$hostname = "http://esdwatchdog.me:5001";
+
+?>
